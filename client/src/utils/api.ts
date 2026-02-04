f3f39b4a5631eb9f1bdbe2efe71ba54837f19435
SHIVANAND VIRANNA NAGANUR <shivanandvn.mca@gmail.com>
2026-02-04 14:27:55 +0530

    updated the  deployed server link

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust as needed
+    baseURL: 'https://svpharma.onrender.com/api', // Adjust as needed
     headers: {
         'Content-Type': 'application/json',
     },
